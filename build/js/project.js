f89db8457b030f76d8681a6e120b8bdbee584581
Torstein Raabye Haugen <torstein.raabye@gmail.com>
2015-03-09 12:05:18 +0100

    Update project.js

--- build/js/project.js
+++ build/js/project.js
@@ -1,28 +1,32 @@
 //Set domain variable because, convenient.
-var domain = "http://oyvind.co/"
+	var domain = "http://oyvind.co/"
 
-//List projects
-var projectList = [
-	domain + "project",
-	domain + "project",
-	domain + "project",
-	domain + "project",
-	domain + "project",
-	domain + "project",
-	domain + "project"
-]
+	//List projects
+	var projectList = [
+		"project1",
+		"project2",
+		"project3",
+		"project4",
+		"project5",
+		"project6",
+		"project7"
+	];
 
-//Index function
-var randomProject = function () {
-	//Count objects in projectList array
-	var indexProjects = Math.floor(Math.random()*projectList.length)
-	//Retrieve an object based on random index
-	/* Code code code */
-	//Return chosen object
-	/* Code code code */
-}
+	//Index function
+	var randomProject = function () {
+		//Count objects in projectList array
+		var indexProject = Math.floor(Math.random()*projectList.length)
+		//Retrieve an object based on random index
+		var url = domain+projectList[indexProject];
+		//Return chosen object
+		return url.toString();
+	}
 
-//Reload page with chosen object
-var loadRandomProject = function () {
-	location.reload(chosenProject);
-}
+	//Reload page with chosen object
+	var loadRandomProject = function () {
+		window.location.href = randomProject();
+	}
+	//If you want on siteload; call loadRandomProject()
+	window.onload = function(){
+		loadRandomProject();
+	};
